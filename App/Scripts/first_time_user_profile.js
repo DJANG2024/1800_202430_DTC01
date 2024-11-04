2dce28849007d1887e0664560bc3fe853a4aa848
Ethan O'Connor <ethan@northnode.ca>
2024-11-03 17:53:36 -0800

    began adding pages for first time user profile

---
+++ App/Scripts/first_time_user_profile.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+//const log = document.querySelector("#log");
+
+form.addEventListener(
+    "submit",
+    (event) => {
+        const data = new FormData(form);
+        let output = "";
+        for (const entry of data) {
+            output = `${output}${entry[0]}=${entry[1]}\r`;
+        }
+        log.innerText = output;
+        event.preventDefault();
+    },
+    false,
+);
+
+z = document.createElement("button")
+z.id = "viewMoreBtn"
+z.innerHTML = "View More!"
+z.classList.add("bg-green-400", "text-white", "rounded", "my-2", "py-2", "px-4",)
