1a6028632b7622d271575d49c9e8db07d7296f2d
Ethan O'Connor <ethan@northnode.ca>
2024-11-04 16:26:29 -0800

    working on first profile

--- App/Scripts/first_time_user_profile.js
+++ App/Scripts/first_time_user_profile.js
@@ -1,21 +1,65 @@
-const form = document.querySelector("form");
-//const log = document.querySelector("#log");
 
-form.addEventListener(
-    "submit",
-    (event) => {
-        const data = new FormData(form);
-        let output = "";
-        for (const entry of data) {
-            output = `${output}${entry[0]}=${entry[1]}\r`;
-        }
-        log.innerText = output;
-        event.preventDefault();
-    },
-    false,
-);
+function process_response() {
 
-z = document.createElement("button")
-z.id = "viewMoreBtn"
-z.innerHTML = "View More!"
-z.classList.add("bg-green-400", "text-white", "rounded", "my-2", "py-2", "px-4",)
+    // x = document.getElementById("description")
+    // y = document.createElement('div')
+    // y.innerHTML = "testing"
+
+    // z = document.createElement("input")
+    // z.classList.add("text-center")
+    // z.innerHTML = "Description"
+    // y.appendChild(z);
+    // x.appendChild(y);
+
+    // z = document.createElement("form")
+
+    // z.id = "description"
+    // z.innerHTML = "View More!"
+
+
+
+    // y.classList.add("text-center")
+    // y.appendChild(z)
+
+    // x.appendChild(y);
+
+    // if (document.getElementById('bordered-radio-1').checked) {
+    //     const x = document.getElementById("description");
+    //     //x.classList.add("hidden")
+    //     x.style.visibility = "visible";
+
+    // } else if (document.getElementById('bordered-radio-2').checked) {
+    //     const x = document.getElementById("description");
+    //     //x.classList.add("visible")
+    //     x.style.visibility = "visible";
+    // }
+
+    var checked = document.getElementById('bordered-radio-1').checked;
+    const x = document.getElementById("description");
+
+    z = document.getElementById('bordered-radio-1')
+    z.addEventListener("click", () => {
+        x.classList.add("hidden")
+        console.log("hidden");
+    });
+
+    y = document.getElementById('bordered-radio-2')
+    y.addEventListener("click", () => {
+        x.classList.remove("hidden")
+        console.log("visible");
+    });
+    // if (checked) {
+    //     const x = document.getElementById("description");
+    //     //x.classList.add("hidden")
+    //     x.style.visibility = "hidden";
+    //     console.log("hidden");
+
+    // }
+    // else if (document.getElementById('bordered-radio-2').checked) {
+    //     const x = document.getElementById("description");
+    //     //x.classList.add("visible")
+    //     x.style.visibility = "visible";
+    //     console.log("visible");
+    // }
+}
+process_response()
