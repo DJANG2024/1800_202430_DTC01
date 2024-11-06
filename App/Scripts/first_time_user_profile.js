a76a0a4735954e79d212e260e62dc2e8361f7ea8
Ethan O'Connor <ethan@northnode.ca>
2024-11-05 19:32:26 -0800

    added js for profile, broken ATM will fix later

--- App/Scripts/first_time_user_profile.js
+++ App/Scripts/first_time_user_profile.js
@@ -38,13 +38,16 @@ function saveUserInfo() {
     if (user) {
         var currentUser = db.collection("users").doc(user.uid);
         var userID = user.uid;
+        var userFullName = user.displayName;
 
         // Get the document for the current user.
         db.collection("profiles").add({
             userID: userID,
+            name: userFullName,
             latitude: lat,
             longitude: long,
-            username: profile_username,  
+            username: profile_username,
+            name: userFullName,  
 
         }).then(() => {
             window.location.href = "../Pages/home.html"; // Redirect to the thanks page
@@ -58,38 +61,38 @@ function saveUserInfo() {
  
 }
 
-function populateUserInfo() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if user is signed in:
-        if (user) {
+// function populateUserInfo() {
+//     firebase.auth().onAuthStateChanged(user => {
+//         // Check if user is signed in:
+//         if (user) {
 
-            //go to the correct user document by referencing to the user uid
-            currentUser = db.collection("users").doc(user.uid)
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    //get the data fields of the user
-                    let userName = userDoc.data().name;
-                    let userSchool = userDoc.data().school;
-                    let userCity = userDoc.data().city;
+//             //go to the correct user document by referencing to the user uid
+//             currentUser = db.collection("users").doc(user.uid)
+//             //get the document for current user.
+//             currentUser.get()
+//                 .then(userDoc => {
+//                     //get the data fields of the user
+//                     let userName = userDoc.data().name;
+//                     let userSchool = userDoc.data().school;
+//                     let userCity = userDoc.data().city;
 
-                    //if the data fields are not empty, then write them in to the form.
-                    if (userName != null) {
-                        document.getElementById("nameInput").value = userName;
-                    }
-                    if (userSchool != null) {
-                        document.getElementById("schoolInput").value = userSchool;
-                    }
-                    if (userCity != null) {
-                        document.getElementById("cityInput").value = userCity;
-                    }
-                })
-        } else {
-            // No user is signed in.
-            console.log("No user is signed in");
-        }
-    });
-}
+//                     //if the data fields are not empty, then write them in to the form.
+//                     if (userName != null) {
+//                         document.getElementById("nameInput").value = userName;
+//                     }
+//                     if (userSchool != null) {
+//                         document.getElementById("schoolInput").value = userSchool;
+//                     }
+//                     if (userCity != null) {
+//                         document.getElementById("cityInput").value = userCity;
+//                     }
+//                 })
+//         } else {
+//             // No user is signed in.
+//             console.log("No user is signed in");
+//         }
+//     });
+// }
 
 // function getInfoNewProfile() {
 //     firebase.auth().onAuthStateChanged(user => {
