9dd702d9b962fb61142a2198660f320b81efd154
Derek Jang <derek.k.jang@gmail.com>
2024-11-06 18:42:13 -0800

    added feed posting.html ID, home.html scripts in head, added home.js function and updated skeleton.js

---
+++ App/Scripts/home.js
@@ -0,0 +1,37 @@
+var currentUser; //points to the document of the user who is logged in
+
+function populateUserInfo() {
+  firebase.auth().onAuthStateChanged((profiles) => {
+    // Check if user is signed in:
+    if (profiles) {
+      //go to the correct user document by referencing to the user uid
+      currentUser = db.collection("profiles").doc(profiles.uid);
+      //get the document for current user.
+      console.log(currentUser);
+      currentUser.get().then((userDoc) => {
+        //get the data fields of the user
+        let profileName = userDoc.data();
+        console.log(profileName);
+        // let profileSchool = userDoc.data().username;
+        // let profileCity = userDoc.data().userID;
+
+        //if the data fields are not empty, then write them in to the form.
+        if (profileName != null) {
+          document.getElementById("nameInput").value = profileName;
+        }
+        // if (userSchool != null) {
+        //   document.getElementById("schoolInput").value = userSchool;
+        // }
+        // if (userCity != null) {
+        //   document.getElementById("cityInput").value = userCity;
+        // }
+      });
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+    }
+  });
+}
+
+//call the function to run it
+populateUserInfo();
